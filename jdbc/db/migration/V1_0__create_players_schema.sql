c6b515bc576238cb619dcaa99b5225d1f8cba664
dickkkkk <105591633+dickkkkk@users.noreply.github.com>
2023-12-12 15:28:00 +0300

    Delete jdbc directory

--- jdbc/db/migration/V1_0__create_players_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS players(
-
-    id        BIGSERIAL NOT NULL,
-    player_id BIGSERIAL NOT NULL PRIMARY KEY,
-    name      varchar(100)
-
-);
--- jdbc/db/migration/V5_0__create_currencies_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS currencies (
-
-    id        BIGSERIAL NOT NULL,
-    player_id BIGSERIAL NOT NULL,
-    name      VARCHAR(100),
-    count     INTEGER,
-
-    FOREIGN KEY (player_id) references players(player_id) ON DELETE CASCADE
-);
